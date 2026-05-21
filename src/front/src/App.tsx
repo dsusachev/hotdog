719ab3d8b92478d0326aadc27a6efafa77903bd0
nekozlovskiy <replyanskiy@icloud.com>
2026-05-21 15:22:12 +0300

    Feature/upload page (#16)
    * Add drag-and-drop, image preview and POST /classify
    
    * feat: connect UploadPage to POST /classify
    
    * Add logo to footer
    
    * Manage logo
    
    * Manage logo
    
    * Manage logo to right
    
    * Please enter the commit message for your changes. Lines starting
    deleted # Изменения, которые будут включены в коммит:
    
    * Add logo to footer
    
    * Add logo to footer
    
    * Manage logo
    
    * Manage logo
    
    * Manage logo to right
    
    * add geolocation request
    
    * fix
    
    ---------
    
    Co-authored-by: Usachev Daniil <d.s.usachev@gmail.com>

--- src/front/src/App.tsx
+++ src/front/src/App.tsx
@@ -10,6 +10,13 @@ import HistoryPage from './pages/HistoryPage'
 import FeedbackPage from './pages/FeedbackPage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import ResultPage from './pages/ResultPage'
+
+const MOCK_RESULT = {
+  label: 'Яблоко',
+  confidence: 0.91,
+  calories: 52,
+}
 
 function App() {
   return (
@@ -26,6 +33,8 @@ function App() {
             <Route path="/feedback"  element={<FeedbackPage />} />
             <Route path="/login"     element={<LoginPage />} />
             <Route path="/register"  element={<RegisterPage />} />
+            <Route path="/result"    element={<ResultPage />} />
+            <Route path="/result/demo" element={<ResultPage mockResult={MOCK_RESULT} />} />
           </Routes>
         </main>
         <Footer />
